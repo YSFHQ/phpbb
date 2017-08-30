1e005235e7988381b4febcf06e38143d37a256ca
Eric Tendian <erictendian@gmail.com>
2017-08-30 00:44:51 +0000

    Refactoring background image selectors to utilize caching of images and responses

--- background_body.php
+++ background_body.php
@@ -1,16 +1,6 @@
 <?php
 
-$base_path = __DIR__.'/styles/ysfhq/theme/images/';
-
-$eventend = new DateTime('2000-01-01T00:00:00Z');
-$now = new DateTime();
-
-// check if we should use a special event banner
-if ($eventend > $now) {
-    $base_path .= 'special_event_backgrounds';
-} else {
-    $base_path .= 'standard_backgrounds';
-}
+$base_path = __DIR__.'/styles/ysfhq/theme/images/standard_backgrounds';
 
 // get the list of files in the dir
 chdir($base_path);
@@ -19,6 +9,5 @@ $custom_images = glob('*.{jpg,png,gif}', GLOB_BRACE);
 // choose a random file in the list
 $file_path = $custom_images[mt_rand(0, count($custom_images) - 1)];
 
-header('Content-type: '.mime_content_type($file_path));
-readfile($file_path);
-
+header('Cache-Control: private, max-age=60');
+header('Location: '.str_replace(__DIR__, '', $base_path.'/'.$file_path));
--- background_header.php
+++ background_header.php
@@ -19,6 +19,5 @@ $custom_images = glob('*.{jpg,png,gif}', GLOB_BRACE);
 // choose a random file in the list
 $file_path = $custom_images[mt_rand(0, count($custom_images) - 1)];
 
-header('Content-type: '.mime_content_type($file_path));
-readfile($file_path);
-
+header('Cache-Control: private, max-age=60');
+header('Location: '.str_replace(__DIR__, '', $base_path.'/'.$file_path));
