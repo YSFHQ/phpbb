f046061a0d34182c87d4ece75b91c248c735d302
Eric Tendian <erictendian@gmail.com>
2018-02-14 01:54:29 +0000

    Adding two-factor auth extension

---
+++ ext/paul999/tfa/event/listener.php
@@ -0,0 +1,178 @@
+<?php
+/**
+*
+* 2FA extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 Paul Sohier
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace paul999\tfa\event;
+
+use paul999\tfa\helper\session_helper_interface;
+use phpbb\config\config;
+use phpbb\db\driver\driver_interface;
+use phpbb\request\request_interface;
+use phpbb\user;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Event listener
+ */
+class listener implements EventSubscriberInterface
+{
+	/**
+	 * @var session_helper_interface
+	 */
+	private $session_helper;
+
+	/**
+	 * @var user
+	 */
+	private $user;
+
+	/**
+	 * @var request_interface
+	 */
+	private $request;
+
+	/**
+	 * @var driver_interface
+	 */
+	private $db;
+
+	/**
+	 * @var config
+	 */
+	private $config;
+
+	/**
+	 * @var string
+	 */
+	private $php_ext;
+
+	/**
+	 * @var string
+	 */
+	private $root_path;
+
+	/**
+	 * Constructor
+	 *
+	 * @access   public
+	 *
+	 * @param session_helper_interface          $session_helper
+	 * @param user                              $user
+	 * @param request_interface                 $request
+	 * @param \phpbb\db\driver\driver_interface $db
+	 * @param \phpbb\config\config              $config
+	 * @param string                            $php_ext
+	 * @param string                            $root_path
+	 *
+	 */
+	public function __construct(session_helper_interface $session_helper, user $user, request_interface $request, driver_interface $db, config $config, $php_ext, $root_path)
+	{
+		$this->session_helper		= $session_helper;
+		$this->user					= $user;
+		$this->request				= $request;
+		$this->config				= $config;
+		$this->db					= $db;
+		$this->php_ext				= $php_ext;
+		$this->root_path			= $root_path;
+	}
+
+	/**
+	 * Assign functions defined in this class to event listeners in the core
+	 *
+	 * @return array
+	 * @static
+	 * @access public
+	 */
+	public static function getSubscribedEvents()
+	{
+		return array(
+			'core.auth_login_session_create_before'		=> 'auth_login_session_create_before',
+			'core.user_setup_after'						=> 'user_setup_after',
+			'core.permissions'			        		=> 'add_permission',
+		);
+	}
+
+	/**
+	 * @param \phpbb\event\data $event
+	 */
+	public function add_permission($event)
+	{
+		$permissions = $event['permissions'];
+		$permissions['a_tfa'] = array('lang' => 'ACL_A_TFA', 'cat' => 'misc');
+		$event['permissions'] = $permissions;
+	}
+
+	/**
+	 * @param \phpbb\event\data $event
+	 */
+	public function user_setup_after($event)
+	{
+		// We skip this when tfa is disabled or we are at a page related to login (This includes logout :))
+		if ($this->config['tfa_mode'] == session_helper_interface::MODE_DISABLED || defined('IN_LOGIN'))
+		{
+			return;
+		}
+		if ($this->user->data['is_bot'] == false && $this->user->data['user_id'] != ANONYMOUS && $this->session_helper->isTfaRequired($this->user->data['user_id'], false, $this->user->data) && !$this->session_helper->isTfaRegistered($this->user->data['user_id']))
+		{
+			$sql = 'SELECT module_id FROM ' . MODULES_TABLE . " WHERE module_langname = 'UCP_TFA' OR module_langname = 'UCP_TFA_MANAGE'";
+			$result = $this->db->sql_query($sql, 3600);
+			$allowed_i = array();
+
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				$allowed_i[] = $row['module_id'];
+			}
+			$this->db->sql_freeresult($result);
+			$ucp_mode = '-paul999-tfa-ucp-tfa_module';
+			$allowed_i[] = $ucp_mode;
+
+			if ($this->user->page['page_name'] === 'ucp.' . $this->php_ext && in_array($this->request->variable('i', ''), $allowed_i))
+			{
+				return; // We are at our UCP page, so skip any other checks. This page is always available
+			}
+			$this->user->add_lang_ext('paul999/tfa', 'common');
+			$url = append_sid("{$this->root_path}ucp.{$this->php_ext}", "i={$ucp_mode}");
+			trigger_error($this->user->lang('TFA_REQUIRED_KEY_MISSING', '<a href="' . $url . '">', '</a>'), E_USER_WARNING);
+
+		}
+	}
+
+	/**
+	 * @param \phpbb\event\data $event
+	 *
+	 * @return \phpbb\event\data $event|null
+	 * @throw http_exception
+	 */
+	public function auth_login_session_create_before($event)
+	{
+		if ($this->config['tfa_mode'] == session_helper_interface::MODE_DISABLED)
+		{
+			return $event;
+		}
+		if (isset($event['login'], $event['login']['status']) && $event['login']['status'] == LOGIN_SUCCESS)
+		{
+			// We have a LOGIN_SUCCESS result.
+			if ($this->session_helper->isTfaRequired($event['login']['user_row']['user_id'], $event['admin'], $event['user_row']))
+			{
+				if (!$this->session_helper->isTfaRegistered($event['login']['user_row']['user_id']))
+				{
+					// While 2FA is enabled, the user has no methods added.
+					// We simply return and continue the login procedure (The normal way :)),
+					// and will disable all pages until he has added a 2FA key.
+					return $event;
+				}
+				else
+				{
+					$this->session_helper->generate_page($event['login']['user_row']['user_id'], $event['admin'], $event['view_online'], !$this->request->is_set_post('viewonline'), $this->request->variable('redirect', ''));
+				}
+			}
+		}
+		return null;
+	}
+}
---
+++ ext/paul999/tfa/modules/module_interface.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ *
+ * 2FA extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 Paul Sohier
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace paul999\tfa\modules;
+
+interface module_interface
+{
+	/**
+	 * Get a language key for this specific module.
+	 * @return string
+	 */
+	public function get_translatable_name();
+
+	/**
+	 * Return the name of the current module
+	 * This is for internal use only
+	 * @return string
+	 */
+	public function get_name();
+
+	/**
+	 * Return if this module is enabled by the admin
+	 * (And all server requirements are met).
+	 *
+	 * Do not return false in case a specific user disabled this module,
+	 * OR if the user is unable to use this specific module,
+	 * OR if a browser specific item is missing/incorrect.
+	 * @return boolean
+	 */
+	public function is_enabled();
+
+	/**
+	 * Check if the current user is able to use this module.
+	 *
+	 * This means that the user enabled it in the UCP,
+	 * And has it setup up correctly.
+	 * This method will be called during login, not during registration/
+	 *
+	 * @param int $user_id
+	 * @return bool
+	 */
+	public function is_usable($user_id);
+
+	/**
+	 * Check if the user can potentially use this.
+	 * This method is called at registration page.
+	 *
+	 * You can, for example, check if the current browser is suitable.
+	 *
+	 * @param int|boolean $user_id Use false to ignore user
+	 * @return bool
+	 */
+	public function is_potentially_usable($user_id = false);
+
+	/**
+	 * Get the priority for this module.
+	 * A lower priority means more chance it gets selected as default option
+	 *
+	 * There can be only one module with a specific priority!
+	 * If there is already a module registered with this priority,
+	 * a Exception might be thrown
+	 *
+	 * @return int
+	 */
+	public function get_priority();
+
+	/**
+	 * Start of the login procedure.
+	 * @param int $user_id
+	 * @return array with data to be assign to the template.
+	 */
+	public function login_start($user_id);
+
+	/**
+	 * Actual login procedure
+	 * @param int $user_id
+	 * @return boolean
+	 */
+	public function login($user_id);
+
+	/**
+	 * If this module can add new keys (Or other things)
+	 *
+	 * @return boolean
+	 */
+	public function can_register();
+
+	/**
+	 * Start with the registration of a new security key.
+	 * This page should return a name of a template, and
+	 * it should assign the required variables for this template.
+	 *
+	 * @return string
+	 */
+	public function register_start();
+
+	/**
+	 * Do the actual registration of a new security key.
+	 *
+	 * @return boolean Result of the registration.
+	 */
+	public function register();
+
+	/**
+	 * This method is called to show the UCP page.
+	 * You can assign template variables to the template, or do anything else here.
+	 */
+	public function show_ucp();
+
+	/**
+	 * Delete a specific row from the UCP.
+	 * The data is based on the data provided in show_ucp.
+	 * @param int $key
+	 * @return void
+	 */
+	public function delete($key);
+}
